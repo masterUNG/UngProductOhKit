6c64203a5e7c413c74d4d24fb949f29c2f040034
masterUNG <phrombutr@gmail.com>
2020-09-20 10:45:59 +0700

    Start UserGit

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+class MyConstant {
+  List<String> positions = [
+    'Account Manager',
+    'Administrative Officer',
+    'Brand Manager',
+    'Customer Service Executive',
+    'Financial Analyst',
+    'HR Generalist/ Specialist',
+    'Other...'
+  ];
+
+  MyConstant();
+}
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  List<Color> primaryColors = [
+    Colors.limeAccent.shade700,
+    Colors.purple.shade600
+  ];
+
+  MyStyle();
+}
